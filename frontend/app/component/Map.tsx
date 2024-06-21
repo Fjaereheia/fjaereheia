398b0d80beea5f350d65b36b9532259e2086a0d6
MichelleGjolberg <msgjolbe@stud.ntnu.no>
2024-06-21 15:47:34 +0200

    diverse oppdateringer

--- frontend/app/component/Map.tsx
+++ frontend/app/component/Map.tsx
@@ -1,31 +1,47 @@
-import mapboxgl from 'mapbox-gl'; 
-import { useEffect, useRef, useState} from 'react';
-mapboxgl.accessToken = 'pk.eyJ1IjoibWljaGVsbGViZWtrIiwiYSI6ImNseG9rNWphaDBkMm0yanM4NDJod3UwenYifQ.fLKf7OmeQGjAMpB2BdgvjA';
+import mapboxgl from "mapbox-gl";
+import { useEffect, useRef, useState } from "react";
+import "app/component/style.map.css"; //Midlertidig styling
+import { Helmet } from "react-helmet"; //sjekk om dette er nødvendig
 
-export default function Map(){
-    //mapboxgl.accessToken = ' <UserAccessToken /> ';
-    
-    const mapContainer = useRef<HTMLDivElement | null>(null);
-    const map = useRef<mapboxgl.Map | null>(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWljaGVsbGViZWtrIiwiYSI6ImNseG9rNWphaDBkMm0yanM4NDJod3UwenYifQ.fLKf7OmeQGjAMpB2BdgvjA"; //Funnet på tutorial, burde bruke egen
 
-    useEffect(() => {
-        if (map.current) return; 
-        map.current = new mapboxgl.Map({
-          container: mapContainer.current,
-          style: 'mapbox://styles/mapbox/streets-v12',
-          center: [lng, lat],
-          zoom: zoom
-        });
-      });
+export default function Map() {
+  //mapboxgl.accessToken = ' <UserAccessToken /> ';
 
-    
-    return (
-        <div>
-            <div ref={mapContainer} className="map-container" />
-        </div>
-        );
+  const mapContainer = useRef<HTMLDivElement | null>(null); //Bedre måte deklarere typer på?
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(8.593782952081346); //Fjærheia koordinater
+  const [lat, setLat] = useState(58.37773188054414);
+  const [zoom, setZoom] = useState(12);
 
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current ?? "", //burde ha standard kart
+      style: "mapbox://styles/mapbox/light-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    if (map.current instanceof mapboxgl.Map) {
+      map.current.addControl(new mapboxgl.NavigationControl());
+    }
+
+    //Legge til marker
+  }, [lng, lat, zoom]);
+  //Når burde den oppdatere seg...
+
+  return (
+    <div className="map-wrapper">
+      <Helmet>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v3.4.0/mapbox-gl.css"
+          rel="stylesheet"
+        />
+        <script src="https://api.mapbox.com/mapbox-gl-js/v3.4.0/mapbox-gl.js"></script>
+      </Helmet>
+      <div ref={mapContainer} className="map-container" />{" "}
+    </div>
+  );
 }
