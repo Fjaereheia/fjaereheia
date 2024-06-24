3c927ed1c68a8f0e6dc74650131417ca71fd6b2a
Martin Bolle <martin.bolle@bekk.no>
2024-06-24 14:31:51 +0200

    added functionallity to retrieve data from frontend

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,9 +1,42 @@
 import type { MetaFunction } from "@remix-run/node";
+import { createClient } from "@sanity/client";
+import groq from 'groq'
+import { useState } from "react";
+import { TITLE_QUERYResult } from "sanity/types";
 
 export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
 };
 
+export const client = createClient({
+  projectId: "0chpibsu",
+  dataset: "development",
+  apiVersion: "2024-06-24"
+})
+
+
+
+const TITLE_QUERY = groq`*[_type=="frontpage"]{Tittel}`
+
+export async function getTitle() {
+  const title = await client.fetch<TITLE_QUERYResult>(TITLE_QUERY)
+  console.log(title[1] , typeof title)
+  return title[1]
+}
+export const loader = async () => {
+  const title = getTitle()
+
+  return title
+};
+
+
 export default function Index() {
-  return <h1>Fjæreheia</h1>;
+  const [title, setTitle] = useState("")
+  getTitle().then(result => setTitle(result.Tittel!))
+  console.log(title)
+
+  return (
+    <h1>{title}</h1>
+  )
+
 }
---
+++ frontend/build/client/assets/_index-irfr_JcO.js
@@ -0,0 +1,1 @@
+import{I as e,m as o}from"./_index-D2ALG8ML.js";import"./jsx-runtime-d4vcKfGz.js";export{e as default,o as meta};
