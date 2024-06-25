d27a7445a1a2b85e83ed7f3a8a0c2e3add6fb10a
emilaukner <emilaukner@gmail.com>
2024-06-25 10:15:37 +0200

    created eventType and added to schemaTypes

--- cms/schemaTypes/index.ts
+++ cms/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import {frontpage} from './frontpage'
 import {articleType} from './articleType'
+import {eventType} from './eventType'
 
-export const schemaTypes = [frontpage, articleType]
+export const schemaTypes = [articleType, eventType, frontpage]
