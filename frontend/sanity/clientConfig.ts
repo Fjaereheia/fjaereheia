09c47f56d441ebd5af4353d6b978c05acbd22f72
emilaukner <emilaukner@gmail.com>
2024-06-26 16:52:32 +0200

    Added .env files to handle sanity variables in sanity client

--- frontend/sanity/clientConfig.ts
+++ frontend/sanity/clientConfig.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-    projectId: "0chpibsu",
-    dataset: "development",
-    apiVersion: "2024-06-24"
-  })
+  projectId: import.meta.env.VITE_SANITY_STUDIO_PROJECT_ID ?? "0chpibsu",
+  dataset: import.meta.env.VITE_SANITY_STUDIO_DATASET ?? "production",
+  apiVersion: "2024-06-24",
+});
