db5719ef0ad37074674adbb81f3d4619c036bcb0
Martin Bolle <martin.bolle@bekk.no>
2024-06-27 14:48:56 +0200

    Changed the format of the link

--- cms/schemaTypes/footerType.ts
+++ cms/schemaTypes/footerType.ts
@@ -1,76 +1,67 @@
-import { defineField, defineType, listenQuery } from "sanity";
+import {defineField, defineType, listenQuery} from 'sanity'
 
 export const footerType = defineType({
-    name: 'footer',
-    title: 'Footer',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'text',
-            title: 'Kontaktinformasjon',
-            type: 'array',
-            of: [
+  name: 'footer',
+  title: 'Footer',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'text',
+      title: 'Kontaktinformasjon',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+        },
+        {
+          type: 'image',
+        },
+      ],
+    }),
+    defineField({
+      name: 'links',
+      title: 'Legg til Link',
+      type: 'array',
+      of: [
+        {
+          name: 'link',
+          type: 'object',
+          title: 'Ekstern lenke',
+          fields: [
+            {
+              name: 'href',
+              type: 'url',
+              title: 'URL',
+            },
+            {
+              title: 'Åpne i ny fane',
+              name: 'blank',
+              description: 'Åpne link i ny fane ved aktivering',
+              type: 'boolean',
+            },
+            {
+              name: 'icon',
+              type: 'object',
+              title: 'Ikon',
+              fields: [
                 {
-                    type: 'block',
+                  name: 'name',
+                  type: 'string',
+                  title: 'Ikonnavn',
+                  description: 'Velg navn for ikon',
+                  options: {
+                    list: [
+                      {title: 'Facebook', value: 'fa-facebook'},
+                      {title: 'Twitter', value: 'fa-twitter'},
+                      {title: 'Instagram', value: 'fa-instagram'},
+                    ],
+                  },
                 },
-                {
-                    type: 'image',
-                },
-            ]
-
-        }),
-        defineField({
-            name: 'links',
-            title: 'Legg til Link',
-            type: 'array',
-            of: [
-                {
-                    type: 'block',
-                    marks: {
-                        annotations: [
-                            {
-                                name: 'link',
-                                type: 'object',
-                                title: 'Ekstern lenke',
-                                fields: [
-                                    {
-                                        name: 'href',
-                                        type: 'url',
-                                        title: 'URL'
-                                    },
-                                    {
-                                        title: 'Åpne i ny fane',
-                                        name: 'blank',
-                                        description: 'Åpne link i ny fane ved aktivering',
-                                        type: 'boolean'
-                                    },
-                                    {
-                                        name: 'icon',
-                                        type: 'object',
-                                        title: 'Ikon',
-                                        fields: [
-                                            {
-                                                name: 'name',
-                                                type: 'string',
-                                                title: 'Ikonnavn',
-                                                description: 'Velg navn for ikon',
-                                                options: {
-                                                    list: [
-                                                        { title: 'Facebook', value: 'fa-facebook' },
-                                                        { title: 'Twitter', value: 'fa-twitter' },
-                                                        { title: 'Instagram', value: 'fa-instagram' },
-                                                    ]
-                                                }
-                                            },
-                                        ]
-
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                }
-            ]
-        }),        
-    ]
+              ],
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 })
