a8c57467d50a7d544c5594d9095dcb2d802931ec
emilaukner <emilaukner@gmail.com>
2024-06-27 17:00:23 +0200

    Moved quoteType to objects folder

--- cms/schemaTypes/index.ts
+++ cms/schemaTypes/index.ts
@@ -3,6 +3,6 @@ import {articleType} from './articleType'
 import {eventType} from './eventType'
 import {roleType} from './roleType'
 import customImage from './objects/customImage'
-import {quoteType} from './quoteType'
+import {quoteType} from './objects/quoteType'
 
 export const schemaTypes = [articleType, eventType, frontpage, roleType, customImage, quoteType]
