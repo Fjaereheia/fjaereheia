e01c5e963579f2b81112faa67a5e6259d033aae2
Martin Bolle <martin.bolle@bekk.no>
2024-07-01 14:28:58 +0200

    Made a link from article-page to the respective event.

--- frontend/app/routes/event.$id.tsx
+++ frontend/app/routes/event.$id.tsx
@@ -5,7 +5,6 @@ import { EVENT_QUERYResult } from "sanity/types";
 import { EVENT_QUERY } from "~/queries/event-queries";
 import urlFor from "app/functions/imageUrlBuilder";
 
-
 export async function loader({ params }: LoaderFunctionArgs) {
   const event = await client.fetch<EVENT_QUERYResult>(EVENT_QUERY, params);
 
