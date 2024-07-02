ad38522c292f3eda3bee7583fb196f5112ecc66f
emilaukner <emilaukner@gmail.com>
2024-07-02 12:21:43 +0200

    Updated frontpage query to take lang params

--- frontend/app/queries/frontpage-queries.ts
+++ frontend/app/queries/frontpage-queries.ts
@@ -1,3 +1,12 @@
 import groq from "groq";
+import { client } from "sanity/clientConfig";
+import { FRONTPAGE_QUERYResult } from "sanity/types";
+
+export async function getFrontpage(params: { lang: string }) {
+  const frontpage = (await client.fetch(
+    `*[_type=="frontpage" && language=="${params.lang}"]{title, preamble, event->{title,preamble ,"imageUrl": image.asset->url, slug , text [] {..., asset-> { _id, url}}} , "imageUrl": image.asset->url}[0]`
+  )) as FRONTPAGE_QUERYResult;
+  return frontpage;
+}
 
 export const FRONTPAGE_QUERY = groq`*[_type=="frontpage" && language=="nb"]{title, preamble, image, text, event->{title,preamble, text, image}}[0]`;
