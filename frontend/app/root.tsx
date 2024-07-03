17d9de6b67fa0a0e15dd942d5be077cc6b7aec03
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-03 10:19:01 +0200

    fixed 404 error

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -5,11 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./styles/tailwind.css";
-
-export function links() {
-  return [{ rel: "stylesheet", href: "/styles/tailwind.css" }];
-}
+import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
