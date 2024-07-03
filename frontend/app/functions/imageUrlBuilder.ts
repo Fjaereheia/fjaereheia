561735b4a44463e9d0f363797b251e246fa9c096
Martin Bolle <martin.bolle@bekk.no>
2024-07-03 12:24:02 +0200

    Modified imgBuilder to take hotspots, and enable focal points.

--- frontend/app/functions/imageUrlBuilder.ts
+++ frontend/app/functions/imageUrlBuilder.ts
@@ -3,7 +3,17 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(client);
 
-export default function urlFor(source: string | undefined) {
+type Hotspot = {
+  x?: number;
+  y?: number;
+};
+
+export default function urlFor(source: string, hotspot?: Hotspot): string {
   if (!source) return "";
+
+  if (hotspot && hotspot.x !== undefined && hotspot.y !== undefined) {
+    return builder.image(source).focalPoint(hotspot?.x, hotspot?.y).url();
+  }
+
   return builder.image(source).url();
 }
