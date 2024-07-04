b1ed821e1ad7b0233d8e3625ba7bf92d5f1309d5
emilaukner <emilaukner@gmail.com>
2024-07-04 16:36:04 +0200

    Updated styling

--- frontend/app/components/NewsletterForm.tsx
+++ frontend/app/components/NewsletterForm.tsx
@@ -46,93 +46,84 @@ function NewsletterForm(props: NewsletterFormProps) {
     <div className="fixed inset-0 flex items-center justify-center">
       <div
         ref={ref}
-        className="bg-newsletter w-auto md:w-1/3 h-2/3 m-4 grid grid-cols-1 grid-rows-8"
+        className="bg-newsletter w-auto md:w-1/3 h-2/3 m-4 grid grid-cols-1 grid-rows-auto"
       >
-        <div className="place-items-center">
-          <p className="text-base p-2">
-            Meld deg på nyhetsbrev fra Bruddet og få eksklusiv info, billetter
-            til redusert pris og andre tilbud!
-          </p>
-          <form>
-            <hr className="border-black" />
-            <div className="space-x-1 p-2">
-              <label htmlFor="first_name">Fornavn*:</label>
-              <input
-                type="text"
-                id="first_name"
-                placeholder="Ola"
-                required
-                value={firstName}
-                className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
-                onChange={(e) => setFirstname(e.target.value)}
-              />
-            </div>
-            <hr className="border-black" />
-            <div className="space-x-1 p-2">
-              <label htmlFor="last_name">Etternavn*:</label>
-              <input
-                type="text"
-                id="last_name"
-                placeholder="Nordmann"
-                required
-                value={lastName}
-                className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
-                onChange={(e) => setLastname(e.target.value)}
-              />
-            </div>
-            <hr className="border-black" />
-            <div className="space-x-1 p-2">
-              <label htmlFor="email">E-post*:</label>
-              <input
-                id="email"
-                type="email"
-                placeholder="eksempel@eksempel.com"
-                required
-                value={email}
-                className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <hr className="border-black" />
-            <div className="space-x-1 p-2">
-              <label htmlFor="tlf">Telefonnummer:</label>
-              <input
-                id="tlf"
-                type="text"
-                placeholder="999 99 999"
-                value={tlf}
-                className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
-                onChange={(e) => setTlf(e.target.value)}
-              />
-            </div>
-            <hr className="border-black" />
-            <div className="space-x-1 p-2">
-              <label htmlFor="post_number">Postnummer:</label>
-              <input
-                id="post_number"
-                type="text"
-                placeholder="1234"
-                value={postNumber}
-                className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
-                onChange={(e) => setPostNumber(e.target.value)}
-              />
-            </div>
-            <hr className="border-black" />
-          </form>
-          <p className="text-xs p-2">
-            Ved å melde meg på nyhetsbrev samtykker jeg til at Brudd AS kan
-            sende meg nyheter, tilbud om billetter og annen nyttig informasjon
-            om Brudd og forestillinger, i kanalene jeg samtykker til under. *
-          </p>
-          <hr className="border-black" />
-          <button
-            type="submit"
-            className="w-full h-full underline"
-            onClick={handleSubmit}
-          >
-            Meld på nyhetsbrev
-          </button>
-        </div>
+        <p className="text-base p-2">
+          Meld deg på nyhetsbrev fra Bruddet og få eksklusiv info, billetter til
+          redusert pris og andre tilbud!
+        </p>
+        <form>
+          <div className="space-x-1 p-2 border-t border-black">
+            <label htmlFor="first_name">Fornavn*:</label>
+            <input
+              type="text"
+              id="first_name"
+              placeholder="Ola"
+              required
+              value={firstName}
+              className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div className="space-x-1 p-2 border-t border-black">
+            <label htmlFor="last_name">Etternavn*:</label>
+            <input
+              type="text"
+              id="last_name"
+              placeholder="Nordmann"
+              required
+              value={lastName}
+              className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+          <div className="space-x-1 p-2 border-t border-black">
+            <label htmlFor="email">E-post*:</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="eksempel@eksempel.com"
+              required
+              value={email}
+              className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="space-x-1 p-2 border-t border-black">
+            <label htmlFor="tlf">Telefonnummer:</label>
+            <input
+              id="tlf"
+              type="text"
+              placeholder="999 99 999"
+              value={tlf}
+              className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
+              onChange={(e) => setTlf(e.target.value)}
+            />
+          </div>
+          <div className="space-x-1 p-2 border-t border-black">
+            <label htmlFor="post_number">Postnummer:</label>
+            <input
+              id="post_number"
+              type="text"
+              placeholder="1234"
+              value={postNumber}
+              className="w-full bg-inherit focus:outline-white focus:outline-none focus:ring-0 placeholder-white"
+              onChange={(e) => setPostNumber(e.target.value)}
+            />
+          </div>
+        </form>
+        <p className="text-xs p-2 border-t border-black">
+          Ved å melde meg på nyhetsbrev samtykker jeg til at Brudd AS kan sende
+          meg nyheter, tilbud om billetter og annen nyttig informasjon om Brudd
+          og forestillinger, i kanalene jeg samtykker til under. *
+        </p>
+        <button
+          type="submit"
+          className="w-full h-full underline border-t border-black"
+          onClick={handleSubmit}
+        >
+          Meld på nyhetsbrev
+        </button>
       </div>
     </div>
   );
