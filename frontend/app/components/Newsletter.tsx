95f22686ab9402c12d01f07f9d727fc884acbcfe
emilaukner <emilaukner@gmail.com>
2024-07-05 15:05:31 +0200

    Updated Newsletter

--- frontend/app/components/Newsletter.tsx
+++ frontend/app/components/Newsletter.tsx
@@ -5,13 +5,10 @@ export default function Newsletter() {
   const [showForm, setShowForm] = useState(false);
   return (
     <div>
-      <p>
-        Knus problemet med å holde deg oppdatert på hva som skjer. Meld deg på
-        vårt{" "}
-        <button className="underline" onClick={() => setShowForm(true)}>
-          nyhetsbrev
-        </button>
-      </p>
+      <p>Knus problemet med å holde deg oppdatert på hva som skjer. </p>
+      <button className="underline" onClick={() => setShowForm(true)}>
+        Meld deg på vårt nyhetsbrev
+      </button>
       {showForm && <NewsletterForm setShowForm={setShowForm} />}
     </div>
   );
