d698c37d9ebfb6077c759aeea1f793f5f0fe2f54
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-05 10:13:32 +0200

    added styling to buttonLink based on url + added frontpage styling

--- frontend/app/components/ButtonLink.tsx
+++ frontend/app/components/ButtonLink.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@remix-run/react";
+import { useLocation } from "@remix-run/react";
 
 interface ButtonProps {
   url: string;
@@ -6,11 +7,16 @@ interface ButtonProps {
 }
 
 export default function Button({ url, buttonText }: ButtonProps) {
+  const location = useLocation();
+  const isHomepage = location.pathname === "/";
+
+  const buttonClass = isHomepage
+    ? " text-white px-4 py-2 rounded font-serif"
+    : "";
+
   return (
-    <>
-      <Link to={url}>
-        <button>{buttonText}</button>
-      </Link>
-    </>
+    <Link to={url}>
+      <button className={buttonClass}>{buttonText}</button>
+    </Link>
   );
 }
