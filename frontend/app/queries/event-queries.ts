8a31dc493c9322bfa2f59fd753aab0fcc6cef5cc
emilaukner <emilaukner@gmail.com>
2024-07-05 09:55:01 +0200

    Added roles to event query

--- frontend/app/queries/event-queries.ts
+++ frontend/app/queries/event-queries.ts
@@ -1,4 +1,4 @@
 import groq from "groq";
 
 export const EVENTS_QUERY = groq`*[_type=="event"]`;
-export const EVENT_QUERY = groq`*[_type=="event" && slug.current == $id][0]`;
+export const EVENT_QUERY = groq`*[_type=="event" && slug.current == $id][0]{..., roles->}`;
