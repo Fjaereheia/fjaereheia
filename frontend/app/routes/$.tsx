0a7c25ddf16df5ea9355368895fde2f3d7ed7e98
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-08 12:51:35 +0200

    added $.tsx file

---
+++ frontend/app/routes/$.tsx
@@ -0,0 +1,13 @@
+export const loader = () => {
+  return (
+    <html>
+      <head>
+        <title>Oh no!</title>
+      </head>
+      <body>
+        <h1>Something went wrong</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+      </body>
+    </html>
+  );
+};
