45e6ac743033d8da6d7ac9889a6883b7772f3a5a
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-09 11:33:24 +0200

    simple styling newsletter + updated condition stickyfooter and header. Added styling program page (#85)

--- frontend/app/components/Newsletter.tsx
+++ frontend/app/components/Newsletter.tsx
@@ -4,8 +4,10 @@ import NewsletterForm from "./NewsletterForm";
 export default function Newsletter() {
   const [showForm, setShowForm] = useState(false);
   return (
-    <div>
-      <p>Knus problemet med å holde deg oppdatert på hva som skjer. </p>
+    <div className="flex flex-col justify-center">
+      <p className="text-center font-serif">
+        Knus problemet med å holde deg oppdatert på hva som skjer.{" "}
+      </p>
       <button className="underline" onClick={() => setShowForm(true)}>
         Meld deg på vårt nyhetsbrev
       </button>
--- frontend/app/routes/event._index.tsx
+++ frontend/app/routes/event._index.tsx
@@ -4,6 +4,7 @@ import { client } from "sanity/clientConfig";
 import { EVENTS_QUERYResult } from "sanity/types";
 import { EVENTS_QUERY } from "~/queries/event-queries";
 import ButtonLink from "~/components/ButtonLink";
+import Newsletter from "~/components/Newsletter";
 
 export async function getEvents() {
   const events = await client.fetch<EVENTS_QUERYResult>(EVENTS_QUERY);
@@ -36,17 +37,22 @@ export default function Events() {
   const data = useLoaderData<typeof loader>() as EVENTS_QUERYResult;
 
   return (
-    <div>
-      <h1>Program</h1>
-      <p>Her er det forestillinger</p>
-      {data.map((event, index) => (
-        <div key={index}>
-          <Link key={event._id} to={event.slug?.current ?? ""}>
-            <h2 className="p-4 hover:bg-blue-50">{event.title}</h2>
-          </Link>
-        </div>
-      ))}
-      <ButtonLink url="/" buttonText="Tilbake til hovedsiden"></ButtonLink>
+    <div className="bg-newsletter min-h-screen flex flex-col items-center text-white relative">
+      <div className="mt-44 text-center">
+        {data.map((event, index) => (
+          <div key={index}>
+            <Link key={event._id} to={event.slug?.current ?? ""}>
+              <p className="p-4 hover:underline font-serif text-2xl lg:text-4xl">
+                {event.title}
+              </p>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-40  text-lg lg:text-xl w-2/3 ">
+        <Newsletter />
+      </div>
+
     </div>
   );
 }
