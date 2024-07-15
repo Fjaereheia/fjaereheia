4f43c4696c91e83f045e75785c73e035b8f7106f
emilaukner <emilaukner@gmail.com>
2024-07-15 08:26:12 +0200

    Removed unused lines frontpage-queries

--- frontend/app/queries/frontpage-queries.ts
+++ frontend/app/queries/frontpage-queries.ts
@@ -1,14 +1,12 @@
 import { Params } from "@remix-run/react";
 import groq from "groq";
 import { client } from "sanity/clientConfig";
-import { FRONTPAGE_QUERYResult } from "sanity/types";
 
 export async function getFrontpage(params: Params<string>) {
   if (!params.lang) {
     params = { lang: "nb" };
   }
   const FRONTPAGE_QUERY = groq`*[_type=="frontpage" && language==$lang]{title, image, language, metaTitle, metaDescription, text, event->{title, text, image, slug, metaTitle, metaDescription}}[0]`;
-
   const frontpage = await client.fetch(FRONTPAGE_QUERY, params);
   return frontpage;
 }
