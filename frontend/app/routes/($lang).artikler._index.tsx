12933c678a4b9bf5ba6fbba177c0348fa2b255f8
emilaukner <emilaukner@gmail.com>
2024-07-15 13:08:26 +0200

    Removed unused import

--- frontend/app/routes/($lang).artikler._index.tsx
+++ frontend/app/routes/($lang).artikler._index.tsx
@@ -2,7 +2,6 @@ import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
 import { Link, useLoaderData, useLocation, useParams } from "@remix-run/react";
 import { ARTICLES_QUERYResult } from "sanity/types";
 import { getArticles } from "~/queries/article-queries";
-import ButtonLink from "~/components/ButtonLink";
 import { useTranslation } from "~/utils/i18n";
 
 export async function loader({ params }: LoaderFunctionArgs) {
