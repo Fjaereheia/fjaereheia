ab04c31690a0686782a3c809327a3d69518d97db
Martin Bolle <martin.bolle@bekk.no>
2024-07-15 14:27:25 +0200

    Updated styling after merging from main

--- frontend/app/routes/($lang).artikler._index.tsx
+++ frontend/app/routes/($lang).artikler._index.tsx
@@ -29,9 +29,8 @@ export default function Articles() {
   const data = useLoaderData<typeof loader>() as ARTICLES_QUERYResult;
   const params = useParams();
   return (
-    <div>
-      <h1>Artikler</h1>
-      <p>Her er det artikler</p>
+    <div flex-col items-center mx-6 mt->
+      <h1 className="flex flex-col items-center text-4xl mb-36">Artikler</h1>
       {data.map((article, index) => (
         <div key={index}>
           <Link
@@ -41,6 +40,7 @@ export default function Articles() {
                 ? "/en/artikler/" + article.slug?.current
                 : article.slug?.current!
             }
+            className="flex flex-col items-center text-2xl p-2"
           >
             <h2 className="p-4 hover:bg-blue-50">{article.title}</h2>
           </Link>
