d449e4e5f083d27a2ab6b209fa9e3728861d1032
emilaukner <emilaukner@gmail.com>
2024-07-15 13:07:34 +0200

    Updated artikler.id to use english and norwegian text

--- frontend/app/routes/($lang).artikler._index.tsx
+++ frontend/app/routes/($lang).artikler._index.tsx
@@ -3,6 +3,7 @@ import { Link, useLoaderData, useLocation, useParams } from "@remix-run/react";
 import { ARTICLES_QUERYResult } from "sanity/types";
 import { getArticles } from "~/queries/article-queries";
 import ButtonLink from "~/components/ButtonLink";
+import { useTranslation } from "~/utils/i18n";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const articles = await getArticles(params);
@@ -28,10 +29,11 @@ export const meta: MetaFunction = () => {
 export default function Articles() {
   const data = useLoaderData<typeof loader>() as ARTICLES_QUERYResult;
   const params = useParams();
+  const { t } = useTranslation();
   return (
     <div>
-      <h1>Artikler</h1>
-      <p>Her er det artikler</p>
+      <h1>{t(texts.header)}</h1>
+      <p>{t(texts.blurb)}</p>
       {data.map((article, index) => (
         <div key={index}>
           <Link
@@ -49,3 +51,14 @@ export default function Articles() {
     </div>
   );
 }
+
+const texts = {
+  header: {
+    en: "Articles",
+    nb: "Artikler",
+  },
+  blurb: {
+    en: "Here there are articles",
+    nb: "Her er det artikler",
+  },
+};
