323f72dedcad40e512eba0c55a6c2b89bfe28343
emilaukner <emilaukner@gmail.com>
2024-07-16 12:55:21 +0200

    Removed redundant text artikler.index

--- frontend/app/routes/($lang).artikler._index.tsx
+++ frontend/app/routes/($lang).artikler._index.tsx
@@ -2,7 +2,6 @@ import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
 import { Link, useLoaderData, useLocation, useParams } from "@remix-run/react";
 import { ARTICLES_QUERYResult } from "sanity/types";
 import { getArticles } from "~/queries/article-queries";
-import { useTranslation } from "~/utils/i18n";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const articles = await getArticles(params);
@@ -28,11 +27,8 @@ export const meta: MetaFunction = () => {
 export default function Articles() {
   const data = useLoaderData<typeof loader>() as ARTICLES_QUERYResult;
   const params = useParams();
-  const { t } = useTranslation();
   return (
     <div>
-      <h1>{t(texts.header)}</h1>
-      <p>{t(texts.blurb)}</p>
       {data.map((article, index) => (
         <div key={index}>
           <Link
@@ -50,14 +46,3 @@ export default function Articles() {
     </div>
   );
 }
-
-const texts = {
-  header: {
-    en: "Articles",
-    nb: "Artikler",
-  },
-  blurb: {
-    en: "Here there are articles",
-    nb: "Her er det artikler",
-  },
-};
