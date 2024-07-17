703a7bfb2cbdc20c72393edbef25310a93f60b63
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-17 11:01:51 +0200

    added button

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -107,6 +107,7 @@ export default function App() {
           }}
         >
           <Header />
+          <LanguageButton />
           <Outlet />
           <StickyFooter infoUrl="/info" programUrl="/event" />
         </motion.div>
