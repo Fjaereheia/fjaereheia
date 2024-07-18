6122b70b062276d7b18d2bc65276e8160ee9da09
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-18 08:30:17 +0200

    adedd /program to new animation

--- frontend/app/utils/pageTransition.ts
+++ frontend/app/utils/pageTransition.ts
@@ -1,8 +1,8 @@
 import { useRef, useEffect } from "react";
 import { useLocation } from "@remix-run/react";
 
-export const usePrevious = (value: any) => {
-  const ref = useRef();
+export const usePrevious = (value: string) => {
+  const ref = useRef<string | undefined>();
   useEffect(() => {
     ref.current = value;
   });
@@ -15,20 +15,30 @@ export const usePageTransition = () => {
 
   let slideDirection = 0;
 
-  switch (location.pathname) {
-    case "/info":
-      slideDirection = previousLocation == "/info" ? 1 : -1;
-      break;
-    case "/event":
-      slideDirection = previousLocation == "/event" ? -1 : 1;
-      break;
-    case "/program":
-      slideDirection = previousLocation == "/event" ? -1 : 1;
-      break;
-    default:
-      slideDirection = previousLocation == "/event" ? -1 : 1;
-      break;
-  }
+  if (location.pathname.includes("/info")) {
+    if (previousLocation?.includes("/artikler")) slideDirection = 1;
+    else slideDirection = -1;
+  } else if (location.pathname.includes("/artikler")) {
+    slideDirection = -1;
+  } else if (location.pathname.includes("/event")) {
+    if (previousLocation?.includes("/event/")) slideDirection = -1;
+    else slideDirection = 1;
+  } else if (location.pathname.includes("/program")) {
+    if (previousLocation?.includes("/event/")) slideDirection = -1;
+    else slideDirection = 1;
+  } else if (location.pathname === "/" || location.pathname === "/en") {
+    if (
+      previousLocation?.includes("/info") ||
+      previousLocation?.includes("/artikler")
+    )
+      slideDirection = 1;
+    else if (
+      previousLocation?.includes("/event") ||
+      previousLocation?.includes("/program")
+    )
+      slideDirection = -1;
+    else slideDirection = 0;
+  } else slideDirection = 0;
 
   return { slideDirection, pathname: location.pathname };
 };
