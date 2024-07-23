eea70745224b3f10166a888926e7a8f71f44c390
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-23 13:05:50 +0200

    added gif to prgrampage both in sanity and remix

--- frontend/app/routes/($lang).program._index.tsx
+++ frontend/app/routes/($lang).program._index.tsx
@@ -1,10 +1,11 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { Link, useLoaderData, useLocation, useParams } from "@remix-run/react";
+import { Link, useLoaderData, useParams } from "@remix-run/react";
 import { useEffect } from "react";
 import { PROGRAMPAGE_QUERYResult } from "sanity/types";
 import Newsletter from "~/components/Newsletter";
 import { getProgramPage } from "~/queries/program-queries";
 import { useBackgroundColor } from "~/utils/backgroundColor";
+import urlFor from "~/utils/imageUrlBuilder";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const programPage = await getProgramPage(params);
@@ -21,6 +22,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
 export default function Program() {
   const data = useLoaderData<typeof loader>() as PROGRAMPAGE_QUERYResult;
   const { setColor } = useBackgroundColor();
+  const gifUrl = urlFor(data?.gif?.asset?._ref || "");
   useEffect(() => {
     setColor("bg-newsletter");
   }, [setColor]);
@@ -29,6 +31,15 @@ export default function Program() {
     <div className="min-h-screen flex flex-col items-center text-white relative pb-36">
       <h1 className="text-5xl font-bold mb-12">{data?.title}</h1>
       <div className="flex flex-col items-center font-normal gap-4 text-xl py-12 px-0">
+        {data?.gif ? (
+          <img
+            src={gifUrl}
+            className="absolute w-2/3 right-[10vw] bottom-[25vh] sm:w-1/3  lg:w-1/4 lg:right-[20vw]  lg:bottom-[10vh]"
+          />
+        ) : (
+          <></>
+        )}
+
         {data?.links?.map((link, index) => (
           <Link
             key={index}
@@ -39,6 +50,7 @@ export default function Program() {
                   }`
                 : ""
             }
+            className="z-10"
           >
             <p className="p-4 hover:underline font-serif text-2xl lg:text-4xl">
               {link.title}
@@ -46,7 +58,7 @@ export default function Program() {
           </Link>
         ))}
       </div>
-      <div className="mt-auto flex flex-col items-center text-lg lg:text-xl w-4/5 lg:w-2/3">
+      <div className="mt-auto flex flex-col items-center text-lg lg:text-xl w-4/5 lg:w-2/3 z-10">
         <Newsletter />
       </div>
     </div>
