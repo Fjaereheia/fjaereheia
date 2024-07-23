ce7fa024344b8dda276fabd23f7c7fc259804676
MichelleGjolberg <102430264+MichelleGjolberg@users.noreply.github.com>
2024-07-23 10:38:47 +0200

    removed buttonlink

--- frontend/app/routes/($lang)._index.tsx
+++ frontend/app/routes/($lang)._index.tsx
@@ -2,7 +2,6 @@ import { LoaderFunctionArgs, json, type MetaFunction } from "@remix-run/node";
 import { useLoaderData, Link, useLocation, useParams } from "@remix-run/react";
 import { FRONTPAGE_QUERYResult } from "sanity/types";
 import { getFrontpage } from "~/queries/frontpage-queries";
-import ButtonLink from "~/components/ButtonLink";
 import urlFor from "~/utils/imageUrlBuilder";
 import PurpleDot from "~/assets/PurpleDot";
 import GreenButton from "~/assets/GreenButton";
@@ -85,20 +84,26 @@ export default function Index() {
             alt={data?.event?.svgTitle?.alt || data?.svgTitle?.alt || "Logo"}
           />
 
-          <div className="flex flex-row justify-center  content-center w-full mt-4">
-            <ButtonLink
-              styling="text-white w-48  text-right px-4 py-2 rounded self-center font-serif text-2xl lg:text-4xl "
-              url={params.lang == "en" ? "/en/info" : "/info"}
-              buttonText="Info"
-            />
+          <div className="flex flex-row justify-center content-center  w-full mt-4">
+            <Link to={params.lang == "en" ? "/en/info" : "/info"}>
+              <button
+                className="text-white w-48  text-right px-4 py-2 rounded self-center font-serif text-2xl lg:text-4xl"
+                aria-label="Info"
+              >
+                Info
+              </button>
+            </Link>
             <div className="mb-4 mt-4 lg:mt-5 mx-1">
               <PurpleDot />
             </div>
-            <ButtonLink
-              styling="text-white w-48 px-4 py-2 text-left rounded self-center font-serif text-2xl lg:text-4xl "
-              url={params.lang == "en" ? "/en/program" : "/program"}
-              buttonText={t(texts.programText)}
-            />
+            <Link to={params.lang == "en" ? "/en/program" : "/program"}>
+              <button
+                className="text-white w-48 px-4 py-2 text-left rounded self-center font-serif text-2xl lg:text-4xl"
+                aria-label={t(texts.programText)}
+              >
+                {t(texts.programText)}
+              </button>
+            </Link>
           </div>
 
           {data?.event && (
