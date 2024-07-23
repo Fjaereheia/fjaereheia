8eef40aed0935c8b3060e0ddafe80e0ebe579482
emilaukner <emilaukner@gmail.com>
2024-07-23 13:59:49 +0200

    Removed quoteblock from RichTextEditor

--- cms/schemaTypes/objects/RichTextEditor.ts
+++ cms/schemaTypes/objects/RichTextEditor.ts
@@ -5,6 +5,13 @@ export default {
   of: [
     {
       type: 'block',
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'h1', value: 'h1'},
+        {title: 'h2', value: 'h2'},
+        {title: 'h3', value: 'h3'},
+        {title: 'h4', value: 'h4'},
+      ],
     },
     {
       type: 'customImage',
