f43c8b304ac80d0a5f38f9f5510e1c8116fedce1
emilaukner <emilaukner@gmail.com>
2024-07-24 15:07:25 +0200

    Updated RichTextEditor to contain all header tags

--- cms/schemaTypes/objects/RichTextEditor.ts
+++ cms/schemaTypes/objects/RichTextEditor.ts
@@ -7,10 +7,12 @@ export default {
       type: 'block',
       styles: [
         {title: 'Normal', value: 'normal'},
-        {title: 'h1', value: 'h1'},
-        {title: 'h2', value: 'h2'},
-        {title: 'h3', value: 'h3'},
-        {title: 'h4', value: 'h4'},
+        {title: 'Heading 1', value: 'h1'},
+        {title: 'Heading 2', value: 'h2'},
+        {title: 'Heading 3', value: 'h3'},
+        {title: 'Heading 4', value: 'h4'},
+        {title: 'Heading 5', value: 'h5'},
+        {title: 'Heading 6', value: 'h6'},
       ],
     },
     {
