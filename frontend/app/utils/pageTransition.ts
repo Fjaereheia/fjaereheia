83101e94f8beb2a42939b8a8af48a815fdf1552c
Martin Bolle <martin.bolle@bekk.no>
2024-07-30 12:03:15 +0200

    Updated animation so it does not slide on change of language

--- frontend/app/utils/pageTransition.ts
+++ frontend/app/utils/pageTransition.ts
@@ -1,11 +1,11 @@
-import { useRef, useEffect } from "react";
-import { useLocation } from "@remix-run/react";
+import { useRef, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "@remix-run/react";
 
 export const usePrevious = (value: string) => {
-  const ref = useRef<string | undefined>();
+  const ref = useRef<string | undefined>(undefined);
   useEffect(() => {
     ref.current = value;
-  });
+  }, [value]);
   return ref.current;
 };
 
@@ -15,30 +15,31 @@ export const usePageTransition = () => {
 
   let slideDirection = 0;
 
-  if (location.pathname.includes("/info")) {
-    if (previousLocation?.includes("/artikler")) slideDirection = 1;
-    else slideDirection = -1;
-  } else if (location.pathname.includes("/artikler")) {
-    slideDirection = -1;
-  } else if (location.pathname.includes("/event")) {
-    if (previousLocation?.includes("/event/")) slideDirection = -1;
-    else slideDirection = 1;
-  } else if (location.pathname.includes("/program")) {
-    if (previousLocation?.includes("/event/")) slideDirection = -1;
-    else slideDirection = 1;
-  } else if (location.pathname === "/" || location.pathname === "/en") {
-    if (
-      previousLocation?.includes("/info") ||
-      previousLocation?.includes("/artikler")
-    )
-      slideDirection = 1;
-    else if (
-      previousLocation?.includes("/event") ||
-      previousLocation?.includes("/program")
-    )
+  if (previousLocation && previousLocation !== location.pathname) {
+    if (location.pathname.includes("/info")) {
+      if (previousLocation.includes("/artikler")) slideDirection = 1;
+      else slideDirection = -1;
+    } else if (location.pathname.includes("/artikler")) {
       slideDirection = -1;
-    else slideDirection = 0;
-  } else slideDirection = 0;
+    } else if (location.pathname.includes("/event")) {
+      if (previousLocation.includes("/event/")) slideDirection = -1;
+      else slideDirection = 1;
+    } else if (location.pathname.includes("/program")) {
+      if (previousLocation.includes("/event/")) slideDirection = -1;
+      else slideDirection = 1;
+    } else if (location.pathname === "/" || location.pathname === "/en") {
+      if (
+        previousLocation.includes("/info") ||
+        previousLocation.includes("/artikler")
+      )
+        slideDirection = 1;
+      else if (
+        previousLocation.includes("/event") ||
+        previousLocation.includes("/program")
+      )
+        slideDirection = -1;
+    }
+  }
 
   return { slideDirection, pathname: location.pathname };
 };
