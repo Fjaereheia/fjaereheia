0ee1eff21dab78673c03b05cf968843163a0e4a7
Martin Bolle <martin.bolle@bekk.no>
2024-08-02 09:52:59 +0200

    Updates heading

--- frontend/app/components/Tickets.tsx
+++ frontend/app/components/Tickets.tsx
@@ -15,7 +15,7 @@ export const Tickets = ({ dateTickets }: Props) => {
   const { t } = useTranslation();
   return (
     <div id="tickets" className="flex flex-col">
-      <h1 className="text-2xl text-white font-sans py-8">{t(texts.tickets)}</h1>
+      <h2 className="text-2xl text-white font-sans py-8">{t(texts.tickets)}</h2>
       {dateTickets?.map((dateTicket: DateTicketType, index) => {
         return <DateTicket key={index} dateTicket={dateTicket} />;
       })}
